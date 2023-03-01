0224f1f7570dae7d321d8ca5a839fafe6755b0f6
aviral kandpal <avikdl@outlook.com>
2023-03-01 12:39:42 +0530

    changes in mongodb

--- helpers/mongo_manager.js
+++ helpers/mongo_manager.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
-exports.connectedToDatabase = () => {
-  mongoose.connect(process.env.MONGOURL, { strictQuery: true }, () => {
-    console.log(`Mongo Db Connected To ${process.env.MONGOURL}`);
-  });
+exports.connectedToDatabase = async () => {
+  try {
+    console.log("================================ Connecting to database");
+    const result = await mongoose.connect(process.env.MONGOURL);
+    console.log("Connected to Mongoose");
+  }catch(e) {
+    console.log(`Error connecting to Mongo ${e.message}`);
+  }
 };
